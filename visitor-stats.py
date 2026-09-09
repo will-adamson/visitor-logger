0f893f0fcfcbb6f6c05c4d4f393745121c620621
Will <adamwd1@student.op.ac.nz>
2026-09-09 17:52:51 +1200

    feat: trying another, there will be more to follow

--- visitor-stats.py
+++ visitor-stats.py
@@ -10,5 +10,16 @@ def load_visits():
     conn.close()
     return visits
 
+def average_distance():
+    conn = sqlite3.connect('logDB.db')
+    c = conn.cursor()
+    c.execute("SELECT AVG(DISTANCE) FROM VISITS")
+    avg_distance = c.fetchone()[0]
+    conn.close()
+    return avg_distance
+
 for visit in load_visits():
     print(visit)
+    
+for distance in average_distance():
+    print(f"Average distance: {distance} cm")

54fbeaa628aabea28f47a97c60eb625e5e7688d5
Will <adamwd1@student.op.ac.nz>
2026-09-09 18:08:57 +1200

    chore: just formatting. should be the last push for the night

--- visitor-stats.py
+++ visitor-stats.py
@@ -13,11 +13,11 @@ def load_visits():
 
 def main():
     visits = load_visits()
-    print(f"Total visits: {len(visits)}")
     
     for visit in visits:
-        print(f"ID: {visit[0]}, DATETIME: {visit[1]}, DISTANCE: {visit[2]}, PHOTO_PATH: {visit[3]}, FACE_DETECTED: {visit[4]}")
+        print(f"ID: {visit[0]}, DATETIME: {visit[1]}, DISTANCE: {visit[2]}, PHOTO_PATH: {visit[3]}")
     
+    print(f"Total visits: {len(visits)}")    
     print("Average distance: {:.2f} cm".format(sum(visit[2] for visit in visits) / len(visits)) if visits else 0)
     print("Average visit per day: {:.2f}".format(len(visits) / len(set(visit[1].split('T')[0] for visit in visits))) if visits else 0)
     

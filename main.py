891fafda3d7f56d29c844ac2da9c0c570789ea6d
Will <adamwd1@student.op.ac.nz>
2026-09-09 13:52:26 +1200

    chore: replaced the test image with one of me, so there's a face to test it on.

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import serial
-import time
 import sqlite3
+import cv2 as cv
+from datetime import datetime
 
 arduino = serial.Serial(port='COM10',  baudrate=9600, timeout=.1)
+conn = sqlite3.connect('logDB.db')
+c = conn.cursor()
 
+try:
+    c.execute('''CREATE TABLE IF NOT EXISTS VISITS
+                 (timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                  DISTANCE REAL,
+                  PHOTO_PATH TEXT,
+                  DETECTED INTEGER)''')
+    while True:
+        data = arduino.readline().decode('utf-8').strip()
+        if data:
+            print(data)
+            row = data.split(',')
+            if len(row) == 2:
+                event_type = row[0]
+                distance = int(row[1])
+                timestamp = datetime.now().isoformat(timespec="seconds")
+                #Both of the following lines are placeholders for once I have photo taking and image detection working
+                face_detected = 1
+                photo_path = "/test.jpg"
 
-while True:
-    line = arduino.readline().decode('utf-8').strip()
-    if line:
-        print(line)
+                c.execute("INSERT INTO VISITS (datetime, distance, photo_path, face_detected) VALUES (?, ?, ?, ?)",
+                        (timestamp, distance, photo_path, face_detected))
+                conn.commit()
+except KeyboardInterrupt:
+    print("\nStopped.")
+finally:
+    arduino.close()
+    conn.close()
+            
